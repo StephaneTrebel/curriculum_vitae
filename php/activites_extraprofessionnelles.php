2fd1b5b2621ae426f3e34a3db08b07ebeefc7709
Stéphane Trébel <stephane.trebel@gmail.com>
2015-05-18 12:04:42 +0200

    Content

--- php/activites_extraprofessionnelles.php
+++ php/activites_extraprofessionnelles.php
@@ -1,6 +1,6 @@
 <h1>Activités Extraprofessionnelles</h1>
 <ul>
-  <li>Administration des sites internet <a href="http://www.stephanetrebel.com"><em>Stephane Trebel</em></a> et <a href="http://www.espritpaleo.com"><em>Esprit Paleo</em></a></li>
+  <li>Sport (Randonnée, Musculation, Cyclisme)</li>
   <li>Photographie (et administration d’une <a href="http://www.stephanetrebel.com/galerie">galerie en ligne</a>)</li>
   <li>Pratique de la Guitare</li>
 </ul>
