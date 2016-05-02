a0cd6fbf9af0edb5c46aa6377af996d23e139d4c
flaoua <stephane.trebel@gmail.com>
2016-05-02 08:09:38 +0200

    Ajout de mon expérience chez Conserto

--- php/dev_web.php
+++ php/dev_web.php
@@ -3,12 +3,32 @@
     <h1>Expérience professionnelle</h1>
     <ul>
       <li>
+        <h2>Aout 2015 - Aujourd'hui</h2>
+        Developpeur Front/Back javascript pour <strong>Conserto</strong>
+        <ul>
+          <li>
+            <h3>2015 - Aujourd'hui</h3>ETG (Code de la route) pour LaPoste:
+            <ul>
+              <li>Réalisation d'une API REST HTTP en nodejs (Swagger, Sequelize, TLS)</li>
+              <li>Réalisation d'une application web réalisée en Angular et Material Design</li>
+            </ul>
+          </li>
+          <li>
+            <h3>Août 2015 - Septembre 2015</h3>Cit'ease Mulhouse pour Kromi/Ineo Digital
+            <ul>
+              <li>Réalisation d'une application iOS réalisée en Angular + Cordova</li>
+              <li>Fonctionnalité principale: mettre en forme des données provenant d'une API d'Ineo Digital</li>
+              <li>Fonctionnalités secondaires: flux twitter, flux d'informations météorologiques</li>
+            </ul>
+          </li>
+      </li>
+       <li>
         <h2>Janvier 2015 – Aujourd'hui</h2>
         Auto-Entreprise <strong>Pr<em>e</em>MIS</strong>, spécialisée dans le consulting en informatique
         <ul>
           <li>Création et administration du site <a href="//www.espritpaleo.com" title="Esprit Paleo">EspritPaleo.com</a></li>
-          <li>Création d'une application web de gestion de budget</li>
-          <li>Auto-formation continue sur SCSS, NodeJS et PHP5</li>
+          <li>Création et administration du site <a href="//nominoeredon.scoutblog.net" title="Le blog des scouts de Redon">Le blog des scouts de Redon</a></li>
+          <li>Réalisation de projets personnels open source sur <a href="//github.com/StephaneTrebel" title="Github">Mon Github</a></li>
         </ul>
       </li>
     </ul>
@@ -17,16 +37,20 @@
   <section class="competences">
     <h1>Compétences</h1>
     <ul>
-      <li>Outils bureautiques : Office, Access, mais aussi LibreOffice, LaTeX ;</li>
-      <li>Technologies maîtrisées : HTML5, CSS3/SCSS, PHP5, jS/jQuery, MySQL/MariaDB, Apache</li>
-      <li>Technologies approchées : HAML, AngularJS, Ember.js</li>
-      <li>Bonne expérience des environnements Windows Server, IBM AS/400 , UNIX/Linux.</li>
+      <li>Frameworks javascript: nodejs (+ES2015), Angular 1.5</li>
+      <li>Frameworks approchés: Ember.js, Backbone, React</li>
+      <li>Autres technos web connues: HTML5, CSS3/SCSS, PHP5, MySQL, Apache</li>
+      <li>Bonne expérience des environnements UNIX/Linux serveur</li>
+      <li>Methode Agile (Scrum), Test Driven Development</li>
+      <li>Forte sensibilité à la programmation fonctionelle et l'état d'esprit DevOps</li>
+      <li>Outils bureautiques: MS Office, Access, et également LibreOffice, LaTeX, Markdown</li>
     </ul>
   </section>
 
   <section class="formation">
     <h1>Formation</h1>
     <ul>
+      <li>Aout 2015: Formation de trois jours à nodejs chez Zenika Nantes</li>
       <li>2000 – 2001 : Licence d’Informatique, mention ‘Assez Bien’ (Université d’Évry Val d’Essonne)</li>
       <li>1998 – 2000 : DEUG MIAS, mention ‘Assez Bien’ (Université d’Évry Val d’Essonne)</li>
       <li>1998 : Baccalauréat S Option Mathématiques, mention ‘Bien’ (Lycée Georges Brassens, Evry)</li>
