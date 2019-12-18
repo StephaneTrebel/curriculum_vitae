ceade475ae50aa61fc78ef4de1abbbae8d912e97
Stéphane Trebel <stephane.trebel@capgemini.com>
2019-12-18 11:24:48 +0100

    Change header info (add desired positions/locations)

--- php/infos_administratives.php
+++ php/infos_administratives.php
@@ -1,12 +1,7 @@
 <!--h1>Informations administratives</h1-->
 <ul>
   <li>Stéphane Trébel</li>
-<!--
-  <li>15 Le Cormier</li>
-  <li>56350 Saint-Vincent-Sur-Oust</li>
-  <li>&nbsp;</li>
-  <li>Mobile : 06 81 62 69 40</li>
-  <li>34 ans</li>
--->
+	<li>Postes recherchés: Tech Lead Typescript, Responsable Technique Web, Tech Lead DevOps</li>
+  <li>Zone de recherche: Rennes, Nantes, Vannes, à distance des gares SNCF</li>
   <li><a href="mailto:stephane.trebel@gmail.com">stephane.trebel@gmail.com</a></li>
 </ul>
