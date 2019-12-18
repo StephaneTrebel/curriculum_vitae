5d8fd3c739375cf4b6727520e123e3960cc55e81
Stéphane Trebel <stephane.trebel@capgemini.com>
2019-12-18 14:08:04 +0100

    Remove now useless flaticon footer mention

--- index.php
+++ index.php
@@ -49,14 +49,6 @@ $activites = "activites_extraprofessionnelles";
 
     <footer>
       Copyright 2019. Tous droits réservés.
-      <div>Icons made by
-        <a href="http://www.flaticon.com/authors/google" title="Google">Google
-        </a> from
-        <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com
-        </a> is licensed by
-        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0
-        </a>
-      </div>
     </footer>
 
   </div> <!-- main_content -->
