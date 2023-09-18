64de457da0967bfe8229a8ed00da77e0c8038ee5
Stéphane TRÉBEL (WeScale) <stephane.trebel@wescale.fr>
2023-09-18 16:38:30 +0200

    Praise the Full Remote !

--- php/infos_administratives.php
+++ php/infos_administratives.php
@@ -2,6 +2,6 @@
 <ul>
   <li>Stéphane Trébel</li>
 	<li>Postes recherchés: Tech Lead Typescript, Responsable Technique Web, Tech Lead DevOps</li>
-  <li>Zone de recherche: Rennes, Nantes, Vannes, à distance des gares SNCF</li>
+  <li>Zone de recherche: Full Remote, ou sinon Rennes, Nantes, Vannes, à proximité des gares SNCF</li>
   <li><a href="mailto:stephane.trebel@gmail.com">stephane.trebel@gmail.com</a></li>
 </ul>
